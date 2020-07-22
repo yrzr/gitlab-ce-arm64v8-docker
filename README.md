6f33ea771cf536cc6823c27a820c315888c64c3c
yrzr <chrishen@hku.hk>
2020-07-22 22:47:20 +0800

    add files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# gitlab-ce-arm64
+# GitLab Community Edition docker image builder for arm64v8
 
-gitlab-ce docker image builder for arm64v8
+Compatible with arm64v8 architectural.
+
+Source build code: https://gitlab.com/gitlab-org/omnibus-gitlab
+
+docker hub: https://hub.docker.com/r/yrzr/gitlab-ce-arm64v8
---
+++ create-tags.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat version_list | while read version; do
+    echo ${version}
+    HAVE_TAG=false
+    for tag in $(git tag); do
+        if [ "${version}" == "${tag}" ]; then
+            HAVE_TAG=true
+        fi
+    done
+    if ! ${HAVE_TAG}; then
+        git tag ${version}
+    fi
+done
