cee5981a20c6e4e795431cdff7880b0a93c6874f
Christopher SHEN <shenleidi@gmail.com>
2023-07-19 23:38:11 +0800

    clean up gitlab-ci

--- latest-tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LATEST=$(git tag | sort -rV | head -n 1)
-
-if [ "${LATEST}" == "${CI_COMMIT_TAG}" ]; then 
-    docker tag ${DOCKER_NS}/${BASENAME}:${CI_COMMIT_TAG} ${DOCKER_NS}/${BASENAME}:latest;
-fi
