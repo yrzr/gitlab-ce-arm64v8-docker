6c3ae8133588d1b673ae1ff2c0e986383c95a926
Christopher SHEN <shenleidi@gmail.com>
2024-10-07 16:57:52 +0800

    check-version.sh: better echo

--- check-version.sh
+++ check-version.sh
@@ -10,3 +10,6 @@ for i in $(seq 1 ${SEARCH_PAGES}); do
     curl -s "https://packages.gitlab.com/gitlab/gitlab-ce?page=${i}" | grep "_arm64.deb" | grep -v '\-rc' | sed 's/.*>\(.*\)<.*/\1/' | sort -u | sed 's/gitlab-ce_\(.*\)_arm64.deb/\1/' >> version_list;
 done
 sort -rVu version_list -o version_list
+
+echo "Latest versions:"
+cat version_list
