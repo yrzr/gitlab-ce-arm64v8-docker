da230086f3c96acae21a34c8ce5ef90b860c8913
Christopher SHEN <shenleidi@gmail.com>
2024-10-07 16:55:34 +0800

    create-push-tags.sh: better echo

--- create-push-tags.sh
+++ create-push-tags.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 cat version_list | while read version; do
-    echo ${version}
     HAVE_TAG=false
+
     for tag in $(git tag); do
         if [ "${version}" == "${tag}" ]; then
             HAVE_TAG=true
+            break
         fi
     done
+
     if ! ${HAVE_TAG}; then
+        echo "Creating and pushing tag ${version}"
         git tag ${version}
         git push origin ${version}
+    else
+        echo "Tag ${version} already exists, skipping"
     fi
 done
